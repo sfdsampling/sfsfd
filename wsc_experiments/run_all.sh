428acc21a11e2c789412b782e57ee1131bad74cb
Tyler <tchang@anl.gov>
2023-03-27 12:16:17 -0500

    updated run script to provide seeds

--- wsc_experiments/run_all.sh
+++ wsc_experiments/run_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd .. && export PYTHONPATH=$PYTHONPATH:`pwd` && cd wsc_experiments
 
-# Run the tests
-python3 compare_3d.py
-python3 compare_4d.py
-python3 compare_5d.py
+# Run the tests with 10 random seeds ( 0--9 )
+python3 compare_3d.py 0 1 2 3 4 5 6 7 8 9
+python3 compare_4d.py 0 1 2 3 4
+python3 compare_4d.py 5 6 7 8 9
+python3 compare_5d.py 0 1
+python3 compare_5d.py 2 3
+python3 compare_5d.py 4 5
+python3 compare_5d.py 6 7
+python3 compare_5d.py 8 9
