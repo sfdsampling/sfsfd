4def9f350a1883335063c8b04522bb3261ee42a1
Tyler <tchang@anl.gov>
2023-03-31 11:27:52 -0500

    testing different values of sfsfd, to figure out low performance

--- wsc_experiments/compare_3d.py
+++ wsc_experiments/compare_3d.py
@@ -7,12 +7,16 @@ import csv
 
 # Problem hyperparams
 dimension_list = np.arange(3,4,1) # 1 levels
-grid_cells_per_dimension = 3 # discretization level of 3
+grid_cells_per_dimension = 4 # discretization level of 3
 sample_size_list = np.arange(10,101,30) # 4 levels
-no_of_iterations_per_perturbation = 10 # starting number of perturbations
+no_of_iterations_per_perturbation = 20 # starting number of perturbations
 adaptive_sample_size = 50 # increase by 1 every 100 iterations
 fieldnames = ["method","discrepancy","maximin",
               "eigen_value","cumulative", "time_to_sol"]
+weights = np.ones(3)
+weights[1] = 0.1
+weights[2] = 0.01
+weights = weights / np.sum(weights)
 
 def comparison(iseed, file_name_csv):
     """ Main driver routine that performs comparison of all 3 techniques. """
@@ -47,14 +51,14 @@ def random_sample(dimension, sample_size, file_name_csv):
     best_dis_random = qmc.discrepancy(best_sample)
     best_maximin_random = maximindist(best_sample)
     best_e_optimality_random = e_optimality(best_sample)
-    best_weighted_criteria = (best_dis_random - best_maximin_random - best_e_optimality_random)/3
+    best_weighted_criteria = np.dot(np.array([best_dis_random, -best_maximin_random, -best_e_optimality_random]), weights)
 
     for i in range(0,20):
         sample = np.random.random_sample((sample_size, dimension))
         dis_random = qmc.discrepancy(sample)
         maximin_random = maximindist(sample)
         e_optimality_random = e_optimality(sample)
-        weighted_criteria = (dis_random - maximin_random - e_optimality_random)/3
+        weighted_criteria = np.dot(np.array([dis_random, -maximin_random, -e_optimality_random]), weights)
         if weighted_criteria < best_weighted_criteria:
             best_sample = sample
             best_weighted_criteria = weighted_criteria
@@ -88,7 +92,8 @@ def sfd_sample(dimension, sample_size, file_name_csv):
         file_name=file_name_csv,
         no_of_iterations_per_perturbation = no_of_iterations_per_perturbation, # Increase with dimension
         adaptive_sample_size = adaptive_sample_size,
-        sample_size = sample_size # This can be user's choice, e.g., 10
+        sample_size = sample_size, # This can be user's choice, e.g., 10
+        weights = weights
         )
     start = time.time()
     # Train the model and save results to file_name_csv
@@ -103,7 +108,7 @@ def latin_hypercube(dimension, sample_size, file_name_csv):
     dis_lhs = qmc.discrepancy(sample)
     maximin_lhs = maximindist(sample)
     e_optimality_lhs = e_optimality(sample)
-    weighted_criteria_lhs = (dis_lhs - maximin_lhs - e_optimality_lhs)/3
+    weighted_criteria_lhs = np.dot(np.array([dis_lhs, - maximin_lhs, - e_optimality_lhs]), weights)
     with open(file_name_csv, "a") as file_instance:
         
         writer = csv.DictWriter(file_instance, fieldnames=fieldnames)
@@ -121,7 +126,7 @@ def sobol_seq(dimension, sample_size, file_name_csv):
     dis_sobol = qmc.discrepancy(sample)
     maximin_sobol = maximindist(sample)
     e_optimality_sobol = e_optimality(sample)
-    weighted_criteria_sobol = dis_sobol-maximin_sobol-e_optimality_sobol
+    weighted_criteria_sobol = np.dot(np.array([dis_sobol, - maximin_sobol, - e_optimality_sobol]), weights)
     with open(file_name_csv, "a") as file_instance:
         
         writer = csv.DictWriter(file_instance, fieldnames=fieldnames)
@@ -130,7 +135,7 @@ def sobol_seq(dimension, sample_size, file_name_csv):
             'discrepancy':dis_sobol,
             'maximin':maximin_sobol,
             'eigen_value':e_optimality_sobol,
-            'cumulative':weighted_criteria_sobol/3
+            'cumulative':weighted_criteria_sobol
         })       
         
 ### Helper function to calculate criteria scores ###
