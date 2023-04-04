47e6e55098dfeb13ef57c1cbae2b550e8c555010
Tyler <tchang@anl.gov>
2023-04-03 20:12:04 -0500

    restructured tests

---
+++ wsc_experiments/run_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##COBALT -n 1
+##COBALT -q gpu_smx2_v100
+##COBALT -A Performance
+##COBALT -t 6:00:00
+##COBALT -I
+#
+#source activate sfd
+#
+#export http_proxy="http://0.0.0.0:8889"
+#export https_proxy="https://0.0.0.0:8889"
+#export ftp_proxy="ftp://0.0.0.0:8889"
+#
+#
+
+cd .. && export PYTHONPATH=$PYTHONPATH:`pwd` && cd wsc_experiments
+
+# Run the tests with 10 random seeds ( 0--9 )
+python3 compare_2d.py 0
+python3 compare_3d.py 0
+python3 compare_4d.py 0
+python3 compare_5d.py 0
+python3 compare_6d.py 0
+python3 compare_7d.py 0
+python3 compare_8d.py 0
+python3 compare_9d.py 0
+python3 compare_10d.py 0
