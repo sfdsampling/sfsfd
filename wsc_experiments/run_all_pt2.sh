24e25944046054d32b6c4c79adb59aaa07dbea31
Tyler <tchang@anl.gov>
2023-04-12 11:26:02 -0500

    added sample run scripts

---
+++ wsc_experiments/run_all_pt2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##COBALT -n 1
+##COBALT -q gpu_smx2_v100
+##COBALT -A Performance
+##COBALT -t 6:00:00
+##COBALT -I
+#
+#source activate sfd
+#
+#export http_proxy="http://0.0.0.0:8889"
+#export https_proxy="https://0.0.0.0:8889"
+#export ftp_proxy="ftp://0.0.0.0:8889"
+#
+#
+
+cd .. && export PYTHONPATH=$PYTHONPATH:`pwd` && cd wsc_experiments
+
+# Run the tests with 10 random seeds ( 0--9 )
+python3 compare_dD.py 5  1 
+python3 compare_dD.py 10 1 
+python3 compare_dD.py 15 1 
+python3 compare_dD.py 20 1 
+python3 compare_dD.py 25 1 
+python3 compare_dD.py 30 1 
